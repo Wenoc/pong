8d37d58e58ed3660c1fd0969826e3235cbc3928a
Simon Cederqvist <simon@tori.fi>
2016-12-26 21:28:00 +0200

    Moved db user/pass things to db.inc and added example

--- src/controllers/DB.php
+++ src/controllers/DB.php
@@ -1,11 +1,7 @@
 <?php
 namespace pong\controllers;
 require __DIR__ . '/../../vendor/autoload.php';
-define('DB_SERVER','localhost');
-define('DB_USERNAME','webuser');
-define('DB_PASSWORD','vrfaehinxh54wonghc7w459gh8cmw45px9gwx89gcmyo5w4hgeb5vsl8gbey4stve45ytdghrsfgcknfgy345ndgfi5ngcfierfxmi8');
-define('DB_DATABASE','pong');
-
+include("../inc/db.inc");
 class Db
 {
     private $db;
